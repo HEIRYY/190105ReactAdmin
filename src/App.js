f6de8a2b93587553ff7b9e207e48f0b66a94a833
yangyang_hlw <yangyang_hlw@atwasoft.net>
2020-10-22 09:37:13 +0800

    文件名修改

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Button,message } from 'antd'
 /*
 * 应用的根组件
 */
-export default class App extends  Component{
+class App extends  Component{
     handleClick=()=>{
         message.success("点我成功了")
     }
 
 
     render(){
+
         return <div>
-            <Button type="primary"  onClick={this.handleClick}>点我</Button>
+            <Button type="default"  onClick={this.handleClick}>点我</Button>
         </div>
     }
 
 }
+export default App
